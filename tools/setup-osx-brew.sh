7a4d691b50753f9906e3d5a2eaf6a7b1aff30a0c
Thomas Voß <thomas@airmap.com>
2017-08-28 16:14:29 +0200

    Properly link the homebrew ssl version

--- tools/setup-osx-brew.sh
+++ tools/setup-osx-brew.sh
@@ -11,3 +11,6 @@ brew install \
   openssl \
   pkgconfig \
   protobuf
+
+ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
+ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
