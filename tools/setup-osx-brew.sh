5f022345f6af0a47c87fff4883c1f9b8f0fa02fe
Thomas Voß <thomas@airmap.com>
2017-08-28 17:34:30 +0200

    Manually link in proper ssl version on OS X

--- tools/setup-osx-brew.sh
+++ tools/setup-osx-brew.sh
@@ -8,9 +8,13 @@ brew install \
   cmake \
   boost \
   cryptopp \
-  openssl \
   pkgconfig \
-  protobuf
+  protobuf \
+  openssl || true
 
+ln -s /usr/local/opt/openssl/include/openssl/ /usr/local/include/
 ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
 ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
+ln -s /usr/local/opt/openssl/lib/pkgconfig/libssl.pc /usr/local/lib/pkgconfig
+ln -s /usr/local/opt/openssl/lib/pkgconfig/libcrypto.pc /usr/local/lib/pkgconfig
+ln -s /usr/local/opt/openssl/lib/pkgconfig/openssl.pc /usr/local/lib/pkgconfig
