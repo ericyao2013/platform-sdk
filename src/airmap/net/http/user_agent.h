9c5c2a22a7aa096122cf7877568d8b94cf500650
Thomas Voß <thomas.voss.bochum@gmail.com>
2017-09-06 21:36:11 +0200

    Add proper versioning and a user-agent string

---
+++ src/airmap/net/http/user_agent.h
@@ -0,0 +1,16 @@
+#ifndef AIRMAP_NET_HTTP_USER_AGENT_H_
+#define AIRMAP_NET_HTTP_USER_AGENT_H_
+
+#include <string>
+
+namespace airmap {
+namespace net {
+namespace http {
+
+std::string user_agent();
+
+}  // namespace http
+}  // namespace net
+}  // namespace airmap
+
+#endif  // AIRMAP_NET_HTTP_USER_AGENT_H_
