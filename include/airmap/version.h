a5fc1bb67af9d6faaf317a7151d69001e632b636
Thomas Voß <thomas.voss.bochum@gmail.com>
2017-09-19 12:08:19 +0200

    Add git revision and build time to version info

--- include/airmap/version.h
+++ include/airmap/version.h
@@ -1,6 +1,9 @@
 #ifndef AIRMAP_VERSION_H_
 #define AIRMAP_VERSION_H_
 
+#include <airmap/date_time.h>
+#include <airmap/optional.h>
+
 #include <cstdint>
 
 namespace airmap {
@@ -11,6 +14,8 @@ struct Version {
   std::uint32_t major;
   std::uint32_t minor;
   std::uint32_t patch;
+  Optional<std::string> git_revision;
+  Optional<DateTime> build_timestamp;
 };
 
 }  // namespace airmap
--- src/airmap/cmds/airmap/cmd/version.cpp
+++ src/airmap/cmds/airmap/cmd/version.cpp
@@ -13,8 +13,9 @@ cmd::Version::Version()
     : cli::CommandWithFlagsAndAction{"version", "prints version information", "prints version information"} {
   action([this](const cli::Command::Context& ctxt) {
     util::FormattingLogger log{create_default_logger(ctxt.cout)};
-    log.infof(component, "airmap version %d.%d.%d", ::airmap::Version::current().major,
-              ::airmap::Version::current().minor, ::airmap::Version::current().patch);
+    log.infof(component, "airmap version %d.%d.%d (git rev.: %s, build ts.: %s)", ::airmap::Version::current().major,
+              ::airmap::Version::current().minor, ::airmap::Version::current().patch,
+              ::airmap::Version::current().git_revision, ::airmap::Version::current().build_timestamp);
     return 0;
   });
 }
