4f2098ff282914e19d3da9133b791308c68baa50
vosst <thomas@airmap.com>
2017-12-05 17:54:08 +0100

    Add command 'end-flight-comms'

---
+++ src/airmap/cmds/airmap/cmd/end_flight_comms.cpp
@@ -0,0 +1,119 @@
+#include <airmap/cmds/airmap/cmd/end_flight_comms.h>
+
+#include <airmap/client.h>
+#include <airmap/codec.h>
+#include <airmap/context.h>
+#include <airmap/paths.h>
+#include <airmap/util/telemetry_simulator.h>
+
+#include <signal.h>
+
+#include <fstream>
+#include <iterator>
+#include <thread>
+
+namespace cli = airmap::util::cli;
+namespace cmd = airmap::cmds::airmap::cmd;
+
+using json = nlohmann::json;
+
+namespace {
+constexpr const char* component{"end-flight-comms"};
+}
+
+cmd::EndFlightComms::EndFlightComms()
+    : cli::CommandWithFlagsAndAction{"end-flight-comms", "end injection of telemetry data",
+                                     "end injection of telemetry data"} {
+  flag(flags::version(params_.version));
+  flag(flags::log_level(params_.log_level));
+  flag(flags::config_file(params_.config_file));
+  flag(flags::token_file(params_.token_file));
+  flag(flags::flight_id(params_.flight_id));
+
+  action([this](const cli::Command::Context& ctxt) {
+    log_ = util::FormattingLogger{create_filtering_logger(params_.log_level, create_default_logger(ctxt.cout))};
+
+    if (!params_.config_file) {
+      params_.config_file = ConfigFile{paths::config_file(params_.version).string()};
+    }
+
+    if (!params_.token_file) {
+      params_.token_file = TokenFile{paths::token_file(params_.version).string()};
+    }
+
+    std::ifstream in_config{params_.config_file.get()};
+    if (!in_config) {
+      log_.errorf(component, "failed to open configuration file %s for reading", params_.config_file);
+      return 1;
+    }
+
+    std::ifstream in_token{params_.token_file.get()};
+    if (!in_token) {
+      log_.errorf(component, "failed to open token file %s for reading", params_.token_file);
+      return 1;
+    }
+
+    auto token = Token::load_from_json(in_token);
+
+    if (!params_.flight_id) {
+      log_.errorf(component, "missing parameter 'flight-id'");
+      return 1;
+    }
+
+    if (!params_.flight_id.get().validate()) {
+      log_.errorf(component, "parameter 'flight-id' for accessing AirMap services must not be empty");
+      return 1;
+    }
+
+    auto result = ::airmap::Context::create(log_.logger());
+
+    if (!result) {
+      log_.errorf(component, "Could not acquire resources for accessing AirMap services");
+      return 1;
+    }
+
+    auto context = result.value();
+    auto config  = Client::load_configuration_from_json(in_config);
+
+    log_.infof(component,
+               "client configuration:\n"
+               "  host:                %s\n"
+               "  version:             %s\n"
+               "  telemetry.host:      %s\n"
+               "  telemetry.port:      %d\n"
+               "  credentials.api_key: %s",
+               config.host, config.version, config.telemetry.host, config.telemetry.port, config.credentials.api_key);
+
+    context->create_client_with_configuration(
+        config, [this, &ctxt, token, context](const ::airmap::Context::ClientCreateResult& result) {
+          if (not result) {
+            log_.errorf(component, "failed to create client: %s", result.error());
+            context->stop(::airmap::Context::ReturnCode::error);
+            return;
+          }
+
+          auto client = result.value();
+
+          client->flights().end_flight_communications(
+              Flights::EndFlightCommunications::Parameters{token.id(), params_.flight_id.get()},
+              [this, &ctxt, context, client](const Flights::EndFlightCommunications::Result& result) {
+                if (!result) {
+                  log_.errorf(component, "failed to end flight communications: %s", result.error());
+                  context->stop(::airmap::Context::ReturnCode::error);
+                  return;
+                }
+
+                log_.infof(component, "successfully ended flight communications");
+                context->stop();
+              });
+        });
+
+    return context->exec({SIGINT, SIGQUIT},
+                         [this, context](int sig) {
+                           log_.infof(component, "received [%s], shutting down", ::strsignal(sig));
+                           context->stop();
+                         }) == ::airmap::Context::ReturnCode::success
+               ? 0
+               : 1;
+  });
+}
