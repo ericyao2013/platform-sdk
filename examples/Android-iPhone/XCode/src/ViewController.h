1c465ef66867cd2cdf88ba96167c85efabb66e4a
Steve Winston <steve.winston@airmap.com>
2018-09-05 21:22:02 -0700

    Add in iOS/iPad client

---
+++ examples/Android-iPhone/XCode/src/ViewController.h
@@ -0,0 +1,12 @@
+//
+//  ViewController.h
+//  airmap
+//
+//  Created by Steven Winston on 10/16/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ examples/Android-iPhone/XCode/src/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  reference
+//
+//  Created by Steven Winston on 10/16/17.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@end
+
+    
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+
+@end
