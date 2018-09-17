5512064aaf6bbf4dbcf2309078473d2d7f28d195
Thomas Voß <me@tvoss.io>
2018-09-18 01:14:48 +0200

    [osx] Drop custom SSL setup

--- tools/osx/setup.sh
+++ tools/osx/setup.sh
@@ -9,13 +9,5 @@ brew install \
   go \
   perl \
   pkgconfig \
-  openssl \
   qt \
   zlib || true
-
-ln -s /usr/local/opt/openssl/include/openssl/ /usr/local/include/ || true
-ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/ || true
-ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/ || true
-ln -s /usr/local/opt/openssl/lib/pkgconfig/libssl.pc /usr/local/lib/pkgconfig || true
-ln -s /usr/local/opt/openssl/lib/pkgconfig/libcrypto.pc /usr/local/lib/pkgconfig || true
-ln -s /usr/local/opt/openssl/lib/pkgconfig/openssl.pc /usr/local/lib/pkgconfig || true
