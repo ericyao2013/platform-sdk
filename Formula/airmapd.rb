74f56e47303b08421e9382b2ab6c8e59fa3a2998
Steven Winston <gpx1000@users.noreply.github.com>
2018-09-25 13:06:25 -0700

    separate out the brew forumla fixes. (#127)

--- Formula/airmapd.rb
+++ Formula/airmapd.rb
@@ -1,6 +1,6 @@
 class Airmapd < Formula
-  homepage "https://github.com/airmap/airmapd"
-  url "https://github.com/airmap/airmapd.git"
+  homepage "https://github.com/airmap/platform-sdk"
+  url "https://github.com/airmap/platform-sdk.git"
   version "0.0.1"
 
   depends_on "cmake" => :build
@@ -8,13 +8,13 @@ class Airmapd < Formula
   depends_on "c-ares"
   depends_on "pkgconfig"
   depends_on "openssl"
-  depends_on "qt@5.7"
+  depends_on "qt"
   depends_on "zlib"
 
   def install
     Dir.mkdir("build")
     Dir.chdir("build")
-    system "cmake","-DCMAKE_INSTALL_PREFIX=#{prefix}","-DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt@5.7/5.7.1",".."
+    system "cmake","-DCMAKE_INSTALL_PREFIX=#{prefix}","-DCMAKE_PREFIX_PATH=/usr/local/opt/qt",".."
     system "make"
     system "make", "install"
   end
